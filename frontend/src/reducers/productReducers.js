ed1315f55da746a9353bef6f0d763d5defcb130e
cybodt <cybodt@live.com>
2021-05-25 20:14:31 +0300

    share code on github

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,27 +1,39 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstants';
+import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstants';
 
-const initialState = {
-  loading: false,
-  products: [],
-  error: ''
-}
-
-export const productListReducer = (state = initialState, action) => {
+export const productListReducer = (state = { loading: true, products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
       return {
-        ...state,
         loading: true
       }
     case PRODUCT_LIST_SUCCESS:
       return {
-        ...state,
         loading: false,
         products: action.payload
       }
     case PRODUCT_LIST_FAIL:
       return {
-        ...state,
+        loading: false,
+        error: action.payload
+      }
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (state = { loading: true, product: {} }, action) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return {
+        loading: true
+      }
+    case PRODUCT_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        product: action.payload
+      }
+    case PRODUCT_DETAILS_FAIL:
+      return {
         loading: false,
         error: action.payload
       }

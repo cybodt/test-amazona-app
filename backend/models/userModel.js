899e574919a633c69ef52c1f36d3590c68cc618f
cybodt <cybodt@live.com>
2021-06-08 12:41:25 +0300

    share code on github

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,8 +1,16 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  email: {type: String, required: true, unique: true},
-  name: {type: String, required: true},
-  isAdmin: {type: Boolean, default: false}
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false, required: true }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+export default User;
